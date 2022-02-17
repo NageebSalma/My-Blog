9d331df6e8972b905661eba222789d12ac4225b9
NageebSalma <sleem.eaas@gmail.com>
2022-02-17 12:51:38 +0200

    changed to default port

--- app.js
+++ app.js
@@ -262,6 +262,6 @@ app.post("/" , (req,res)=>{
   res.redirect("/")
 })
 
-app.listen(3000, function() {
+app.listen( process.env.PORT|| 3000, function() {
   console.log("Server started on port 3000");
 });
